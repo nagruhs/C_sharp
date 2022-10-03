4169e7fb0a0f4af6ecb238a5f18978a43a9c6099
Alexandr R <nagruhs@mail.ru>
2022-10-04 01:44:27 +0500

    вписал комментарии к 8 задаче

--- Seminar_1/Homework/Task_8/Program.cs
+++ Seminar_1/Homework/Task_8/Program.cs
@@ -5,14 +5,14 @@
 Console.Clear();
 
 Console.Write("Введите число: ");
-int n = int.Parse(Console.ReadLine()!);
-int index = 1;
+int n = int.Parse(Console.ReadLine()!); //в переменную n записываем введенное (конечное) число
+int index = 1; // переменная index (искомые числа) начинается с 1 по условию
 
-while (index <= n)
+while (index <= n) // цикл пока index меньше или равно введенного значения
 {
-    if (index % 2 == 0)
+    if (index % 2 == 0) // если остаток от деления текущего числа на 2 равен нулю
     {
-        Console.Write($"{index} ");
+        Console.Write($"{index} "); // то выводим это число
     }
-    index++;
+    index++; // переходим к следующему числу
 }
