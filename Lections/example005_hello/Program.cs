33ad23f3196a9ba8e1e08301b73be100141c0e9e
Alexandr R <nagruhs@mail.ru>
2022-10-09 18:11:16 +0500

    объединил лекции и семинары

---
+++ Lections/example005_hello/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Ура это же Маша!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+
+}
---
+++ Lections/example_008/Program.cs
@@ -0,0 +1,52 @@
+﻿// //метод 1 - ничего не принимает и не возвращает
+// void Method1()
+// {
+//     Console.WriteLine("Автор");
+// }
+
+// Method1();
+
+// //***************************************
+
+// // метод 2 - принимает и не возвращает
+// void Method2(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+
+// Method2(msg:"автор",count: 4);// - можно указывать явно какому аргументу даем значение 
+
+// // ***************************************
+
+// // метод 3 - возвращает но не принимает
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int year = Method3();
+// Console.WriteLine(year);
+
+// //***************************************
+
+// //метод 4 - возвращает и принимает
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "йцукен");
+// Console.WriteLine(res);
