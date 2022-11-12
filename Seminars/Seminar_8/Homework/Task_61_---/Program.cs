39c2bb855262a179dee2586c9c4e19e4e6036da9
Alexandr R <nagruhs@mail.ru>
2022-11-12 06:51:20 +0500

    +1 решение с лекции

--- Seminars/Seminar_8/Homework/Task_61_---/Program.cs
+++ Seminars/Seminar_8/Homework/Task_61_---/Program.cs
@@ -44,46 +44,94 @@ PascalTriangle(n);
 
 
 
-// // мои первые попытки.. равносторонний получился(
-// void PascalTriangle(int n)
+
+
+// // решение из лекций
+// int[,] FillTriangle(int row)
 // {
-
-//     int temp = 0;
-//     for (int line = 1; line <= n; line++)
+//     int[,] triangle = new int[row, row];
+//     for (int i = 0; i < row; i++)
 //     {
-//         SpaceNice(n - temp / 2);
-//         int num = 1;
+//         triangle[i, 0] = 1;
+//         triangle[i, i] = 1;
+//     }
 
-//         for (int i = 1; i <= line; i++)
+//     for (int i = 2; i < row; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
 //         {
+//             triangle[i, j] = triangle[i - 1, j - 1] + triangle[i - 1, j];
+//         }
+//     }
+//     return triangle;
+// }
 
-//             Console.Write($"{num}\t");
-//             num = num * (line - i) / i;
+// void Magic(int[,] triangle)
+// {
+//     const int cellWidth = 3;
+//     int col = cellWidth * triangle.GetLength(0);
+//     {
+//         for (int i = 0; i < triangle.GetLength(0); i++)
+//         {
+//             for (int j = 0; j <= i; j++)
+//             {
+//                 Console.SetCursorPosition(col, i + 1);
+//                 if (triangle[i, j] != 0) Console.Write($"{triangle[i, j],cellWidth}");
+//                 // if (triangle[i, j] % 2 != 0) Console.Write("*"); // бешеный треугольник
+//                 col += cellWidth * 2;
+//             }
 
-
+//             col = cellWidth * triangle.GetLength(0) - cellWidth * (i + 1);
+//             Console.WriteLine();
 //         }
 
-//         Console.WriteLine();
-//         temp++;
 //     }
+
 // }
 
-// void SpaceNice(int n)
-// {
-//     int space = 0;
-//     while (space < n)
-//     {
-//         Console.Write(" \t");
-//         space++;
-//     }
-// }
-
-// // Console.Clear();
-// Console.Write("Введите количество строк: ");
+// Console.WriteLine("Введите нужное количество строк треугольника Паскаля:");
 // int n = int.Parse(Console.ReadLine()!);
-// PascalTriangle(n);
+// Console.Clear();
+// Magic(FillTriangle(n));
 
 
 
 
+// // // мои первые попытки.. равносторонний получился(
+// // void PascalTriangle(int n)
+// // {
 
+// //     int temp = 0;
+// //     for (int line = 1; line <= n; line++)
+// //     {
+// //         SpaceNice(n - temp / 2);
+// //         int num = 1;
+
+// //         for (int i = 1; i <= line; i++)
+// //         {
+
+// //             Console.Write($"{num}\t");
+// //             num = num * (line - i) / i;
+
+
+// //         }
+
+// //         Console.WriteLine();
+// //         temp++;
+// //     }
+// // }
+
+// // void SpaceNice(int n)
+// // {
+// //     int space = 0;
+// //     while (space < n)
+// //     {
+// //         Console.Write(" \t");
+// //         space++;
+// //     }
+// // }
+
+// // // Console.Clear();
+// // Console.Write("Введите количество строк: ");
+// // int n = int.Parse(Console.ReadLine()!);
+// // PascalTriangle(n);
